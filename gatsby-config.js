e2819aa1cbbce1202a295b69418c99882f54b041
jameelfullstackdev <jameel@jameelahmed.com.pk>
2020-07-08 16:09:02 +0500

    integrated with Wordpress Post

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,24 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+          // Specify the URL of the WordPress source
+          baseUrl: `http://fullstackdevpro.work/wordpress-api`,
+          protocol: `http`,
+          // Indicates if a site is hosted on WordPress.com
+          hostingWPCOM: false,
+          // Specify which URL structures to fetch
+          includedRoutes: [
+            '**/pages',
+            '**/posts',
+            '**/tags',
+            '**/categories'
+          ]
+      }
+  },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
